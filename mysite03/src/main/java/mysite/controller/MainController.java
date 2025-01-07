ca978f4c125689d7670f0d539032cb52ddc7aee7
ymj07168 <ymj07168@gmail.com>
2025-01-07 09:27:41 +0900

    1-1 application scope: interceptor 사용해서 siteVo 관리

--- mysite03/src/main/java/mysite/controller/MainController.java
+++ mysite03/src/main/java/mysite/controller/MainController.java
@@ -5,23 +5,13 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import jakarta.servlet.http.HttpServletRequest;
-import mysite.service.SiteService;
-import mysite.vo.SiteVo;
 
 @Controller
 public class MainController {
 	
-	private final SiteService siteService;
-	
-	public MainController(SiteService siteService) {
-		this.siteService = siteService;
-	}
-	
 	@RequestMapping({"/", "/main"})
 	public String index(Model model, HttpServletRequest request) {
 		
-		SiteVo siteVo = siteService.getSite();
-		model.addAttribute("siteVo", siteVo);
 		
 		return "main/index";
 	}
--- mysite03/src/main/java/mysite/interceptor/SiteInterceptor.java
+++ mysite03/src/main/java/mysite/interceptor/SiteInterceptor.java
@@ -5,19 +5,24 @@ import org.springframework.web.servlet.LocaleResolver;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import mysite.service.SiteService;
 
 public class SiteInterceptor implements HandlerInterceptor {
 
 	private LocaleResolver localeResolver;
+	private SiteService siteService;
 	
-	public SiteInterceptor(LocaleResolver localeResolver) {
+	public SiteInterceptor(LocaleResolver localeResolver, SiteService siteService) {
 		this.localeResolver = localeResolver;
+		this.siteService = siteService;
 	}
 	
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
+		request.setAttribute("siteVo", siteService.getSite());
 		
+		// locale
 		String lang = localeResolver.resolveLocale(request).getLanguage();
 		request.setAttribute("lang", lang);
 		
