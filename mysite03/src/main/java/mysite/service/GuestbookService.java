957e3a922a6e1b6074d56a41b934d5111375a911
ymj07168 <ymj07168@gmail.com>
2025-01-08 13:55:59 +0900

    transaction #1

--- mysite03/src/main/java/mysite/service/GuestbookService.java
+++ mysite03/src/main/java/mysite/service/GuestbookService.java
@@ -3,26 +3,32 @@ package mysite.service;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import mysite.repository.GuestbookLogRepository;
 import mysite.repository.GuestbookRepository;
 import mysite.vo.GuestbookVo;
 
 @Service
 public class GuestbookService {
 	private GuestbookRepository guestbookRepository;
+	private GuestbookLogRepository guestbookLogRepository;
 	
-	public GuestbookService(GuestbookRepository guestbookRepository) {
+	public GuestbookService(GuestbookRepository guestbookRepository, GuestbookLogRepository guestbookLogRepository) {
 		this.guestbookRepository = guestbookRepository;
+		this.guestbookLogRepository = guestbookLogRepository;
 	}
 	
 	public List<GuestbookVo> getContentsList(){
 		return guestbookRepository.findAll();
 	}
 	
+	@Transactional
 	public int deleteContents(Long id, String password) {
 		return guestbookRepository.deleteByIdAndPassword(id, password);
 	}
 	
+	@Transactional
 	public void addContents(GuestbookVo vo) {
 		guestbookRepository.insert(vo);
 	}
