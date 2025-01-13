ca9c1bf96b50d38a32303b9ed03b7f2606acb446
ymj07168 <ymj07168@gmail.com>
2025-01-13 14:50:08 +0900

    webconfig

--- mysite04/src/main/java/mysite/config/WebConfig.java
+++ mysite04/src/main/java/mysite/config/WebConfig.java
@@ -3,9 +3,13 @@ package mysite.config;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.context.annotation.Import;
+
+import mysite.config.web.MvcConfig;
 
 @Configuration
 @EnableAspectJAutoProxy
+@Import({MvcConfig.class})
 @ComponentScan({"mysite.controller", "mysite.exception"})
 public class WebConfig {
 	
